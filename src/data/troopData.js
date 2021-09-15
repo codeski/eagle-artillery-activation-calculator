4995a27cf9d6cd619b460f0bc870ad0802c9404b
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-09-14 21:50:58 -0600

    added all elixir troops

--- src/data/troopData.js
+++ src/data/troopData.js
@@ -1,12 +1,31 @@
 export const exlixirTroops = [
-    {name: 'barbarian', pic: '/images/elixirTroops/Barbarian.webp', space: 1}, 
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
-    {name: 'archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
+    {name: 'Barbarian', pic: '/images/elixirTroops/Barbarian.webp', space: 1}, 
+    {name: 'Archer', pic: '/images/elixirTroops/Archer.webp', space: 1},
+    {name: 'Giant', pic: '/images/elixirTroops/Giant.webp', space: 5},
+    {name: 'Goblin', pic: '/images/elixirTroops/Goblin.webp', space: 1},
+    {name: 'Wall Breaker', pic: '/images/elixirTroops/Wall_Breaker.webp', space: 2},
+    {name: 'Balloon', pic: '/images/elixirTroops/Balloon.webp', space: 5},
+    {name: 'Wizard', pic: '/images/elixirTroops/Wizard.webp', space: 4},
+    {name: 'Healer', pic: '/images/elixirTroops/Healer.webp', space: 14},
+    {name: 'Dragon', pic: '/images/elixirTroops/Dragon.jpg', space: 20},
+    {name: 'P.E.K.K.A', pic: '/images/elixirTroops/P.E.K.K.A.webp', space: 25},
+    {name: 'Baby Dragon', pic: '/images/elixirTroops/Baby_Dragon.webp', space: 10},
+    {name: 'Miner', pic: '/images/elixirTroops/Miner.webp', space: 6},
+    {name: 'Electro Dragon', pic: '/images/elixirTroops/Electro_Dragon.webp', space: 30},
+    {name: 'Yeti (Yetimite)', pic: '/images/elixirTroops/Yeti.webp', space: 18},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+]
+
+export const DEtroops = () => [
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
+    {name: 'Dragon Rider', pic: '/images/elixirTroops/Dragon_Rider.webp', space: 25},
 ]
 
 export const Spells = [
