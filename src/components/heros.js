861228aaaf07cd18d22a6aed38aa4689ce602032
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-10-14 15:58:42 -0600

    cleaned up hero component code

--- src/components/heros.js
+++ src/components/heros.js
@@ -9,33 +9,21 @@ const Heros = () => {
     const [chosenTroops, setChosenTroops] = useState([])
 
     useEffect(() => {
-        let nums = []
-        chosenTroops.forEach(troop => {
-            nums.push(troop.space)
-        })
-
-        let total = nums.reduce((a, b) => {
-            return a + b
+        let total = chosenTroops.reduce((a, b) => {
+            return a + b.space
         }, 0)
-        
         setHeroTotal(total)
-
     },[chosenTroops])
 
     const handleClick = (e, hero) => {
-        // console.log(hero)
         if  (!e.currentTarget.disabled) {
-            console.log(e.target)
-            // e.target.disabled = true
             e.currentTarget.disabled = true
             setChosenTroops(prevChosenTroops => prevChosenTroops.concat(hero))
-            console.log(chosenTroops)} 
+        } 
     }
 
     const resetButton = (e) => {
-        console.log("YO What's up?")
         let selected = document.querySelectorAll("#heroButton")
-        // console.log("this is selected", selected)
         selected.forEach(troop => troop.disabled = false)
         setHeroTotal(0)
         setChosenTroops([])
@@ -54,7 +42,6 @@ const Heros = () => {
             )}
             <button onClick={(e) => resetButton(e)}>Reset</button>
             <p>{heroTotal}</p>
-            {/* {console.log(chosenTroops)} */}
         </div>  
     )
 }
